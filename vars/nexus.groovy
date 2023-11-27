038ab81ce9acfbaa3d37688603d51d433f92cb48
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-27 16:05:24 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,6 +1,6 @@
-def pushtoNexus( String nexus_username,String nexus_password,String nexus_url,String nexus_repoId,String packageName){
+def pushtoNexus( String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,String NEXUS_REPO_ID,String PACKAGE_NAME){
 
  def currentVersion = version.readVersion("package.json")
- sh "curl -v -u ${nexus_username}:${nexus_password} --upload-file dist.tar.gz ${nexus_url}/repository/${nexus_repoId}/${packageName}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}.tar.gz"
+ sh "curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file dist.tar.gz ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}.tar.gz"
  echo "Artifact deployed to Nexus with version ${currentVersion}"
 }

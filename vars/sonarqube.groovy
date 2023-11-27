089082fce37ed1252851fe172497df76906b8490
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-27 12:30:43 +0530

    Create sonarqube.groovy

---
+++ vars/sonarqube.groovy
@@ -0,0 +1,16 @@
+def sonarscan(String credentialsId, String installationName,String projectName,String projectKey){
+  withSonarQubeEnv(credentialsId: ${credentialsId}, installationName: ${installationName}) {
+    sh '''$SCANNER_HOME/bin/sonar-scanner \
+         -Dsonar.projectKey=${projectName} \
+         -Dsonar.projectName=${projectKey} \
+         -Dsonar.sources=src/ \
+         -Dsonar.tests=testresults/junit \
+         -Dsonar.java.binaries=target/classes/ \
+         -Dsonar.exclusions=src/test/java/****/*.java \
+         -Dsonar.java.libraries=/var/lib/jenkins/.m2/**/*.jar \
+         -Dsonar.projectVersion=${BUILD_NUMBER}-${GIT_COMMIT_SHORT}'''
+
+
+
+  }
+}

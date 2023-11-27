b72435140368e1aa903b19d8b710c856249c8066
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-27 16:35:53 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -2,8 +2,7 @@ def pushtoNexus( String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,St
 
  def currentVersion = version.readVersion("package.json")
  //sh "curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file dist.tar.gz ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}.tar.gz"
-   sh """
-        curl -v -u \${NEXUS_USERNAME}:\${NEXUS_PASSWORD} --upload-file dist.tar.gz \${NEXUS_URL}/repository/\${NEXUS_REPO_ID}/\${PACKAGE_NAME}/\${currentVersion}/\${PACKAGE_NAME}-\${currentVersion}.\${env.BUILD_ID}.tar.gz
-    """
+    sh 'curl -v -u ' + "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" + ' --upload-file dist.tar.gz ' + "${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}.tar.gz"
+
  echo "Artifact deployed to Nexus with version ${currentVersion}"
 }

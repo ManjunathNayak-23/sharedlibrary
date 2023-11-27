e3ae8812324b1c2d1f71f1b12eddf934aa426956
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-27 12:36:02 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -1,16 +1,13 @@
-def sonarscan(String credentialsId, String installationName,String projectName,String projectKey){
-  withSonarQubeEnv(credentialsId: ${credentialsId}, installationName: ${installationName}) {
-    sh '''$SCANNER_HOME/bin/sonar-scanner \
-         -Dsonar.projectKey=${projectName} \
-         -Dsonar.projectName=${projectKey} \
+def sonarscan(String credentialsId, String installationName, String projectName, String projectKey) {
+  withSonarQubeEnv(credentialsId: credentialsId, installationName: installationName) {
+    sh """$SCANNER_HOME/bin/sonar-scanner \
+         -Dsonar.projectKey=${projectKey} \
+         -Dsonar.projectName=${projectName} \
          -Dsonar.sources=src/ \
          -Dsonar.tests=testresults/junit \
          -Dsonar.java.binaries=target/classes/ \
-         -Dsonar.exclusions=src/test/java/****/*.java \
+         -Dsonar.exclusions=src/test/java/**/*.java \
          -Dsonar.java.libraries=/var/lib/jenkins/.m2/**/*.jar \
-         -Dsonar.projectVersion=${BUILD_NUMBER}-${GIT_COMMIT_SHORT}'''
-
-
-
+         -Dsonar.projectVersion=${BUILD_NUMBER}-${GIT_COMMIT_SHORT}"""
   }
 }

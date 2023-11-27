66421fcc70a22c53fa39c702c9a6df575797ab4d
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-27 17:59:22 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -3,8 +3,10 @@ def pushtoNexus( String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,St
 
  def currentVersion = version.readVersion("package.json")
  //sh "curl -v -u admin:admin --upload-file dist.tar.gz http://34.42.7.89:8081/repository/mvpnodejs/mvp-nodejs/1.0.0/mvp-nodejs-1.0.0.175.tar.gz"
-    sh "curl -v -u \${NEXUS_USERNAME}:\${NEXUS_PASSWORD} --upload-file dist.tar.gz \${NEXUS_URL}/repository/\${NEXUS_REPO_ID}/\${PACKAGE_NAME}/\${currentVersion}/\${PACKAGE_NAME}-\${currentVersion}.\${env.BUILD_ID}.tar.gz"
-
+   // sh "curl -v -u \${NEXUS_USERNAME}:\${NEXUS_PASSWORD} --upload-file dist.tar.gz \${NEXUS_URL}/repository/\${NEXUS_REPO_ID}/\${PACKAGE_NAME}/\${currentVersion}/\${PACKAGE_NAME}-\${currentVersion}.\${env.BUILD_ID}.tar.gz"
+sh """
+        curl -v -u \${NEXUS_USERNAME}:\${NEXUS_PASSWORD} --upload-file dist.tar.gz \${NEXUS_URL}/repository/\${NEXUS_REPO_ID}/\${PACKAGE_NAME}/\${currentVersion}/\${PACKAGE_NAME}-\${currentVersion}.${env.BUILD_ID}.tar.gz
+    """
 //sh "set +x curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file dist.tar.gz ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}.tar.gz"
  echo "Artifact deployed to Nexus with version ${currentVersion}"
 }

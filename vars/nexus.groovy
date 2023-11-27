1cbc823a951de1176c25a2a46d0db82032c6a8bd
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-27 15:39:07 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,4 +1,4 @@
-def pushtoNexus( String nexus_username,String nexus_password,String nexus_url,String nexus_repoId,String packageName,String currentVersion){
+def pushtoNexus( String nexus_username,String nexus_password,String nexus_url,String nexus_repoId,String packageName){
 
  def currentVersion = version.readVersion("package.json")
  sh "curl -v -u ${nexus_username}:${nexus_password} --upload-file dist.tar.gz ${nexus_url}/repository/${nexus_repoId}/${packageName}/${currentVersion}/${PACKAGE_NAME}-${currentVersion}.${env.BUILD_ID}.tar.gz"

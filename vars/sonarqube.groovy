35a2642e8854a9daaeb55fb79909d448856ba550
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-27 12:58:46 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -1,11 +1,11 @@
 def sonarscan(String projectName, String projectKey) {
-    sh """$SCANNER_HOME/bin/sonar-scanner \
+         sh '''$SCANNER_HOME/bin/sonar-scanner \
          -Dsonar.projectKey=${projectKey} \
          -Dsonar.projectName=${projectName} \
          -Dsonar.sources=src/ \
          -Dsonar.tests=testresults/junit \
          -Dsonar.java.binaries=target/classes/ \
-         -Dsonar.exclusions=src/test/java/**/*.java \
+         -Dsonar.exclusions=src/test/java/****/*.java \
          -Dsonar.java.libraries=/var/lib/jenkins/.m2/**/*.jar \
-         -Dsonar.projectVersion=${BUILD_NUMBER}-${GIT_COMMIT_SHORT}"""
+         -Dsonar.projectVersion=${BUILD_NUMBER}-${GIT_COMMIT_SHORT}'''
 }

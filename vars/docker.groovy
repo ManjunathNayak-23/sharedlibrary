7b5e1730645c51f81d8d4a672e735758543ff677
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-28 10:33:30 +0530

    Create docker.groovy

---
+++ vars/docker.groovy
@@ -0,0 +1,11 @@
+def buildAndPush(String IMAGE_NAME,String BUILD_ID,String DOCKERFILE_PATH,String DOCKER_HUB_CREDENTIALS){
+
+  dockerImage = docker.build("${IMAGE_NAME}:${BUILD_ID}", "-f ${DOCKERFILE_PATH} .")
+      
+                  
+                          docker.withRegistry('https://registry.hub.docker.com', "${DOCKER_HUB_CREDENTIALS}") {
+                              
+                              dockerImage.push()
+
+
+}

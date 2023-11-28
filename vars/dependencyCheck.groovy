326196589ff626f850f1f137a2aedc434f311eee
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-11-28 10:50:40 +0530

    Create dependencyCheck.groovy

---
+++ vars/dependencyCheck.groovy
@@ -0,0 +1,9 @@
+def owaspDependencyCheck(){
+dependencyCheck additionalArguments: ''' 
+                            -o './'
+                            -s './'
+                            -f 'ALL' 
+                            --prettyPrint''', odcInstallation: 'OWASP Dependency-Check Vulnerabilities'
+                
+                dependencyCheckPublisher pattern: 'dependency-check-report.xml'
+}

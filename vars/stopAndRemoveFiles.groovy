ff74d1fa72e684bb2485280d4e2cc38f6e47d2a1
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-12-01 18:00:23 +0530

    Create stopAndRemoveFiles.groovy

---
+++ vars/stopAndRemoveFiles.groovy
@@ -0,0 +1,11 @@
+def stopRemove(String SSHCONFIGNAME){
+
+ sshPublisher(publishers: [sshPublisherDesc(configName: SSHCONFIGNAME , transfers: [
+                                    sshTransfer(
+                                        execCommand: "sudo systemctl stop nginx && sudo rm -rf /var/www/html/*",
+                                        execTimeout: 120000
+                                    )
+                                ])
+                    ])
+
+}

eecb5c85010d04fe83d8a9bad9a6b6b9cabdc946
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-12-01 17:37:20 +0530

    Update downloadNexusArtifact.groovy

--- vars/downloadNexusArtifact.groovy
+++ vars/downloadNexusArtifact.groovy
@@ -1,6 +1,6 @@
 def download(String OUTPUTFILENAME,String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,String NEXUS_REPO_ID,String PACKAGE_NAME,String APP_VERSION){
 
-  sh "curl -v -o ${OUTPUTFILENAME} -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/1.0.1/${PACKAGE_NAME}-${APP_VERSION}.tar.gz"                    }
+  sh "curl -v -o ${OUTPUTFILENAME} -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/1.0.1/${PACKAGE_NAME}-${APP_VERSION}.tar.gz"                    
 
 
 }

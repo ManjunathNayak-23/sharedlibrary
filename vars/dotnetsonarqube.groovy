5ff65c4cb0f1b2311e716b4946cf81e93a73c7e0
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-12-14 09:57:57 +0530

    Update dotnetsonarqube.groovy

--- vars/dotnetsonarqube.groovy
+++ vars/dotnetsonarqube.groovy
@@ -1,5 +1,5 @@
-def scan(String projectName,String scannerHome){
-  def scannerHome = tool '${scannerHome}'
+def scan(String projectName,String scannerName){
+  def scannerHome = tool '${scannerName}'
       sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll begin /k:\"${projectName}\""
       sh "dotnet build"
       sh "dotnet ${scannerHome}/SonarScanner.MSBuild.dll end"

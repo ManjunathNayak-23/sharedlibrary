6aec67e45fb95c6b2936d0258591b8a8fb337e72
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-12-14 10:10:58 +0530

    Update dotnetNexus.groovy

--- vars/dotnetNexus.groovy
+++ vars/dotnetNexus.groovy
@@ -1,4 +1,4 @@
-def push(String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,String NEXUS_REPO_ID,String PACKAGE_NAME,String buidID){
+def pushToNexus(String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,String NEXUS_REPO_ID,String PACKAGE_NAME,String buidID){
 
       def curlCommand = """
                     curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file artifact.tar.gz ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/${PACKAGE_NAME}-${buidID}.tar.gz        

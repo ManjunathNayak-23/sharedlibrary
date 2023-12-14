d8792c21bf975989b5e1b6c1c4d6f8b2e70ad0e5
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-12-14 10:33:46 +0530

    Update dotnetNexusTask.groovy

--- vars/dotnetNexusTask.groovy
+++ vars/dotnetNexusTask.groovy
@@ -4,9 +4,6 @@ def pushToNexus(String NEXUS_USERNAME,String NEXUS_PASSWORD,String NEXUS_URL,Str
                     curl -v -u ${NEXUS_USERNAME}:${NEXUS_PASSWORD} --upload-file artifact.tar.gz ${NEXUS_URL}/repository/${NEXUS_REPO_ID}/${PACKAGE_NAME}/${PACKAGE_NAME}-${env.BUILD_ID}.tar.gz        
               """
          sh curlCommand
-       echo "Artifact deployed to Nexus with version ${env.BUILD_ID}"
- def deployedVersion = "${env.BUILD_ID}"
- echo "Deployed Version: ${deployedVersion}"
- return deployedVersion
+ 
 
 }

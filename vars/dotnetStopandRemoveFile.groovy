601cc9c1c38cc267be31ec169e11091125922cb5
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-12-18 18:25:43 +0530

    Update dotnetStopandRemoveFile.groovy

--- vars/dotnetStopandRemoveFile.groovy
+++ vars/dotnetStopandRemoveFile.groovy
@@ -2,7 +2,7 @@ def stopRemove(String SSHCONFIGNAME){
 
  sshPublisher(publishers: [sshPublisherDesc(configName: SSHCONFIGNAME , transfers: [
                                     sshTransfer(
-                                        execCommand: "sudo fuser -k 5000/tcp && sudo rm -rf /opt/dotnet/",
+                                        execCommand: "fuser -k 5000/tcp && sudo rm -rf /opt/dotnet/",
                                         execTimeout: 120000
                                     )
                                 ])

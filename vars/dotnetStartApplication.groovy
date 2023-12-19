c7c16ee62eb8e18fb61c8d3b609593229ac5b4ec
Manjunath Nayak <65599992+ManjunathNayak-23@users.noreply.github.com>
2023-12-19 11:58:25 +0530

    Update dotnetStartApplication.groovy

--- vars/dotnetStartApplication.groovy
+++ vars/dotnetStartApplication.groovy
@@ -2,7 +2,7 @@ def startService(String SSHCONFIGNAME){
 
 sshPublisher(publishers: [sshPublisherDesc(configName: SSHCONFIGNAME, transfers: [
                                     sshTransfer(
-                                        execCommand: "sudo cp -rf /home/ubuntu/dotnet /opt/ && sudo rm -rf /home/ubuntu/dotnet && cd /home/ubuntu/dotnet/bin/Release/net6.0/ &&  dotnet MongoDBDotNET6.dll",
+                                        execCommand: "sudo cp -rf /home/ubuntu/bin /opt/ && sudo rm -rf /home/ubuntu/bin && cd /opt/bin/Release/net6.0/ &&  dotnet MongoDBDotNET6.dll",
                                         execTimeout: 120000
                                     )
                                 ])
